9da9fafee842f0edc5e481157208228907975358
dishaaggarwal26 <aggarwaldisha26@gmail.com>
2025-05-03 22:22:11 +0530

    added schema

--- src/main/java/com/jobdhundo/Job/Dhundo/repositories/TestimonialRepository.java
+++ src/main/java/com/jobdhundo/Job/Dhundo/repositories/TestimonialRepository.java
@@ -3,5 +3,6 @@ package com.jobdhundo.Job.Dhundo.repositories;
 import com.jobdhundo.Job.Dhundo.entities.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TestimonialRepository extends JpaRepository<Testimonial, Long> {
+public interface TestimonialRepository extends JpaRepository<Testimonial, Integer> {
 }
+
